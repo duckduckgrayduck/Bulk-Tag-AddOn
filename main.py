d72b23e54ba2cf560ebc2f41dc6f8698c0391077
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-08-08 16:54:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This Add-On allows users to add tags or key/value pairs in bulk.
 """
 
 from documentcloud.addon import AddOn
 
-
-class HelloWorld(AddOn):
+class BulkTag(AddOn):
     """An example Add-On for DocumentCloud."""
 
     def main(self):
         """The main add-on functionality goes here."""
         # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        key = self.data.get("key")
+        value = self.data.get("value")
+    
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            document.data[key] = value
+            document.save()
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    BulkTag().main()
